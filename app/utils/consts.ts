ff4f8cca0e446b5da0b4131502e3b5f1e297f29b
Nicholas McGuire <nickmcguire.zero@gmail.com>
2024-05-18 16:08:23 -0500

    Add slack channel + allowlist

--- app/utils/consts.ts
+++ app/utils/consts.ts
@@ -1,3 +1,7 @@
 export const IS_LOCAL = process.env.MODE === 'local';
 
 export const siteUrl = process.env.SITE_URL ?? 'http://localhost:3000';
+
+export const SLACK_CHANNEL = process.env.SLACK_CHANNEL;
+
+export const ALLOWLISTED_FIDS = process.env.ALLOWLISTED_FIDS?.split(',') ?? [];
