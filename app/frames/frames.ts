7aa66f9b7e70914bf06e33c4e90411de9b570f3a
Nicholas McGuire <nickmcguire.zero@gmail.com>
2024-05-18 15:52:48 -0500

    Add middleware

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -1,5 +1,23 @@
 import {createFrames} from 'frames.js/next';
+import {farcasterHubContext} from 'frames.js/middleware';
+
+import {IS_LOCAL} from '../utils/consts';
 
 export const frames = createFrames({
   basePath: '/frames',
+  middleware: [
+    // Using airstack for validation.
+    farcasterHubContext({
+      hubHttpUrl: IS_LOCAL
+        ? 'http://localhost:3010/hub'
+        : 'https://hubs.airstack.xyz',
+      hubRequestOptions: IS_LOCAL
+        ? undefined
+        : {
+            headers: {
+              'x-airstack-hubs': process.env.AIRSTACK_API_KEY as string,
+            },
+          },
+    }),
+  ],
 });
